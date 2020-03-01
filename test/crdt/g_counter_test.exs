5036d62f80779da91abbab0632a424cd354384cd
Robert Narvaez <narvaez.rm@gmail.com>
2020-03-01 19:55:51 +0000

    add g-counter tests

---
+++ test/crdt/g_counter_test.exs
@@ -0,0 +1,21 @@
+defmodule CRDT.GCounterTest do
+  use ExUnit.Case
+  doctest CRDT.GCounter
+  alias CRDT.GCounter, as: GC
+
+  test "initializes 5 elements" do
+    assert GC.initialize(5) == [0, 0, 0, 0, 0]
+  end
+
+  test "updates the set" do
+    assert GC.update({:increment, [0, 0, 0], 2}) == [0, 0, 1]
+  end
+
+  test "compares two sets" do
+    assert GC.compare([0, 1], [0, 0]) == true
+  end
+
+  test "merges two sets" do
+    assert GC.merge([1, 0], [0, 1]) == [1, 1]
+  end
+end
