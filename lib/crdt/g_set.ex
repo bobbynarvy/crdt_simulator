4b7b07ce5fb4a1e26ba4033a94fbd41e713d6bb6
Robert Narvaez <narvaez.rm@gmail.com>
2020-03-03 23:29:22 +0000

    implement GSet and format

---
+++ lib/crdt/g_set.ex
@@ -0,0 +1,42 @@
+defmodule CRDT.GSet do
+  @moduledoc """
+  Implements a Grow-only set CRDT
+  """
+
+  import MapSet
+
+  @doc """
+  Initializes an empty set
+  """
+  def initialize() do
+    new([])
+  end
+
+  @doc """
+  Adds an element to the set
+  """
+  def update({:add, set, elem}) do
+    put(set, elem)
+  end
+
+  @doc """
+  Checks if an element is a member of a set
+  """
+  def query({:lookup, set, elem}) do
+    member?(set, elem)
+  end
+
+  @doc """
+  Checks if a set is the subset of another set
+  """
+  def compare(set1, set2) do
+    subset?(set1, set2)
+  end
+
+  @doc """
+  Merges two sets
+  """
+  def merge(set1, set2) do
+    union(set1, set2)
+  end
+end
