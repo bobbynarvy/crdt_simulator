49f6e9ba6585eac39576cc61f02e4065be3e1a7d
Robert Narvaez <narvaez.rm@gmail.com>
2020-03-08 12:59:31 +0000

    add PNCounterServer test

---
+++ test/crdt/pn_counter_server_test.exs
@@ -0,0 +1,69 @@
+defmodule CRDT.PNCounterServerTest do
+  use ExUnit.Case
+  doctest CRDT.PNCounterServer
+  import CRDT.PNCounterServer
+
+  test "initializes a counter" do
+    {status, _} = start_link(2, 0)
+    assert status == :ok
+  end
+
+  test "queries the payload of the counter" do
+    {:ok, pid} = start_link(2, 0)
+    assert query(pid, :payload) == {[0, 0], [0, 0]}
+  end
+
+  test "queries the value of the counter" do
+    {:ok, pid} = start_link(2, 0)
+    assert query(pid, :value) == 0
+  end
+
+  test "increments the value of the counter" do
+    {:ok, pid} = start_link(2, 0)
+    {status, _ } = update(pid, {:increment})
+    assert status == :ok
+    assert query(pid, :value) == 1
+  end
+
+  test "decrements the value of the counter" do
+    {:ok, pid} = start_link(2, 0)
+    update(pid, {:decrement})
+    assert query(pid, :value) == -1
+  end
+
+  test "a counter compares its value with another" do
+    {:ok, pid1} = start_link(2, 0)
+    {:ok, pid2} = start_link(2, 1)
+    {status, _ } = update(pid1, {:increment})
+    assert status == :ok
+    assert compare(pid1, pid2) == false
+  end
+
+  test "a counter merges its payload with another counter's" do
+    {:ok, pid1} = start_link(2, 0)
+    {:ok, pid2} = start_link(2, 1)
+    
+    update(pid1, {:increment})
+    
+    {status, _} = merge(pid1, pid2)
+    assert status == :ok
+    assert query(pid1, :value) == 1
+
+    {status, _} = merge(pid2, pid1)
+    assert status == :ok
+    assert query(pid2, :value) == 1
+    
+    update(pid2, {:increment})
+    merge(pid1, pid2)
+    assert query(pid1, :value) == 2
+
+    update(pid1, {:increment})
+    assert query(pid1, :value) == 3
+    
+    update(pid2, {:decrement})
+    assert query(pid2, :value) == 1
+
+    merge(pid1, pid2)
+    assert query(pid1, :value) == 2
+  end
+end
