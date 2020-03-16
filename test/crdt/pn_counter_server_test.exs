953ef4dbe4825e9502029d01fe36a245e471d56d
Robert Narvaez <narvaez.rm@gmail.com>
2020-03-16 23:38:09 +0000

    modify start_link params and add test context

--- test/crdt/pn_counter_server_test.exs
+++ test/crdt/pn_counter_server_test.exs
@@ -3,63 +3,62 @@ defmodule CRDT.PNCounterServerTest do
   doctest CRDT.PNCounterServer
   import CRDT.PNCounterServer
 
+  setup do
+    {_, pid} = start_link({2, 0})
+    {:ok, pid: pid}
+  end
+
   test "initializes a counter" do
-    {status, _} = start_link(2, 0)
+    {status, _} = start_link({2, 0})
     assert status == :ok
   end
 
-  test "queries the payload of the counter" do
-    {:ok, pid} = start_link(2, 0)
-    assert query(pid, :payload) == {[0, 0], [0, 0]}
+  test "queries the payload of the counter", ctx do
+    assert query(ctx.pid, :payload) == {[0, 0], [0, 0]}
   end
 
-  test "queries the value of the counter" do
-    {:ok, pid} = start_link(2, 0)
-    assert query(pid, :value) == 0
+  test "queries the value of the counter", ctx do
+    assert query(ctx.pid, :value) == 0
   end
 
-  test "increments the value of the counter" do
-    {:ok, pid} = start_link(2, 0)
-    assert update(pid, {:increment}) == :ok
-    assert query(pid, :value) == 1
+  test "increments the value of the counter", ctx do
+    assert update(ctx.pid, {:increment}) == :ok
+    assert query(ctx.pid, :value) == 1
   end
 
-  test "decrements the value of the counter" do
-    {:ok, pid} = start_link(2, 0)
-    update(pid, {:decrement})
-    assert query(pid, :value) == -1
+  test "decrements the value of the counter", ctx do
+    update(ctx.pid, {:decrement})
+    assert query(ctx.pid, :value) == -1
   end
 
-  test "a counter compares its value with another" do
-    {:ok, pid1} = start_link(2, 0)
-    {:ok, pid2} = start_link(2, 1)
-    update(pid1, {:increment})
-    assert compare(pid1, pid2) == false
+  test "a counter compares its value with another", ctx do
+    {:ok, pid2} = start_link({2, 1})
+    update(ctx.pid, {:increment})
+    assert compare(ctx.pid, pid2) == false
   end
 
-  test "a counter merges its payload with another counter's" do
-    {:ok, pid1} = start_link(2, 0)
-    {:ok, pid2} = start_link(2, 1)
+  test "a counter merges its payload with another counter's", ctx do
+    {:ok, pid2} = start_link({2, 1})
 
-    update(pid1, {:increment})
+    update(ctx.pid, {:increment})
 
-    assert merge(pid1, pid2) == :ok
-    assert query(pid1, :value) == 1
+    assert merge(ctx.pid, pid2) == :ok
+    assert query(ctx.pid, :value) == 1
 
-    assert merge(pid2, pid1) == :ok
+    assert merge(pid2, ctx.pid) == :ok
     assert query(pid2, :value) == 1
 
     update(pid2, {:increment})
-    merge(pid1, pid2)
-    assert query(pid1, :value) == 2
+    merge(ctx.pid, pid2)
+    assert query(ctx.pid, :value) == 2
 
-    update(pid1, {:increment})
-    assert query(pid1, :value) == 3
+    update(ctx.pid, {:increment})
+    assert query(ctx.pid, :value) == 3
 
     update(pid2, {:decrement})
     assert query(pid2, :value) == 1
 
-    merge(pid1, pid2)
-    assert query(pid1, :value) == 2
+    merge(ctx.pid, pid2)
+    assert query(ctx.pid, :value) == 2
   end
 end
