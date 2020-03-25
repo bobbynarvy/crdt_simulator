f244303ec6eaf2e1e560e253326a79b28c718971
Robert Narvaez <narvaez.rm@gmail.com>
2020-03-25 23:48:05 +0000

    move tests from ReplicaBroadcasterTest into a separate test suite

---
+++ test/crdt/registry_test.exs
@@ -0,0 +1,40 @@
+defmodule CRDT.RegistryTest do
+  use ExUnit.Case
+  alias CRDT.Registry, as: R
+
+  describe "when initializing" do
+    test "returns ok" do
+      {status, _} = R.start_link(:pn_counter, 3)
+      assert status == :ok
+      assert List.length(R.replicas()) == 3
+    end
+
+    test "throws error there are less than 3 replicas" do
+      {status, _} = R.start_link(:pn_counter, 1)
+      assert status == :error
+    end
+  end
+
+  setup do
+    :ok = R.start_link(:pn_counter, 3)
+    {:ok}
+  end
+
+  test "keeps track of the CRDT type" do
+    type = R.crdt_type()
+    assert type == :pn_counter
+  end
+
+  test "gets a replica pid by index" do
+    pid = R.replicas(0)
+    assert Process.alive?(pid) == true
+  end
+
+  test "stops all replicas" do
+    pid = R.replicas(0)
+    status = R.stop_replicas()
+    assert Process.alive?(pid) == false
+    assert status == :ok
+    assert List.length(R.replicas()) == 0
+  end
+end
