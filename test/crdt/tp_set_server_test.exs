c4f5a4d0f294032f52c466de7abe33825aa656b6
Robert Narvaez <narvaez.rm@gmail.com>
2020-04-23 23:13:02 +0000

    add TPSetServerTest

---
+++ test/crdt/tp_set_server_test.exs
@@ -0,0 +1,57 @@
+defmodule CRDT.TPSetServerTest do
+  use ExUnit.Case
+  import CRDT.TPSetServer
+
+  setup do
+    {:ok, pid} = start_link({})
+    {:ok, pid2} = start_link({})
+    {:ok, pid: pid, pid2: pid2}
+  end
+
+  test "initialize a two-phase set" do
+    {status, _} = start_link({})
+    assert status == :ok
+  end
+
+  test "queries the payload of the two-phase set", ctx do
+    {add, remove} = query(ctx.pid, :payload)
+
+    assert MapSet.equal?(add, MapSet.new())
+    assert MapSet.equal?(remove, MapSet.new())
+  end
+
+  test "adds and element and looks up a value in the two-phase set", ctx do
+    update(ctx.pid, {:add, :hello})
+    {add, _} = query(ctx.pid, :payload)
+
+    assert MapSet.equal?(add, MapSet.new([:hello]))
+    assert query(ctx.pid, {:lookup, :hello}) == true
+  end
+
+  test "removes an element from a two-phase set", ctx do
+    update(ctx.pid, {:add, :hello})
+    update(ctx.pid, {:remove, :hello})
+    {add, remove} = query(ctx.pid, :payload)
+
+    assert MapSet.equal?(add, MapSet.new([:hello]))
+    assert MapSet.equal?(remove, MapSet.new([:hello]))
+    assert query(ctx.pid, {:lookup, :hello}) == false
+  end
+
+  test "compares two two-phase sets", ctx do
+    update(ctx.pid2, {:add, :world})
+    assert compare(ctx.pid, ctx.pid2) == true
+
+    update(ctx.pid, {:add, :hello})
+    assert compare(ctx.pid, ctx.pid2) == false
+  end
+
+  test "merges two two-phase sets", ctx do
+    update(ctx.pid, {:add, :hello})
+    update(ctx.pid2, {:add, :world})
+    merge(ctx.pid, ctx.pid2)
+
+    assert query(ctx.pid, {:lookup, :hello}) == true
+    assert query(ctx.pid, {:lookup, :world}) == true
+  end
+end
